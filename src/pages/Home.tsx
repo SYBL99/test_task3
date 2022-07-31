4cef1da28e6f50fde455d4b0dc97c36f0d108d18
Дмитрий <dmitriy.shupikov@gmail.com>
2022-07-31 14:36:03 +0700

    API response split

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams, useParams } from "react-router-dom";
+import getAllPosts from "../API/GetAllPosts";
 import Search from "../components/Search";
 import TableHeader from "../components/TableHeader";
+import { APIResponseTypes } from "../interfaces/APITypes"
 
 function Home () {
     const [searchParams, setSearchParams] = useSearchParams();
     const {data} = useParams()
-    console.log(searchParams.getAll('foo'))
-    async function getData (page: number) {
-        const repsponse = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${searchParams.get('jopa') }`)
-        const data = await repsponse.json()
-        console.log(data)
+    const [table, setTable] = useState<APIResponseTypes[]>([])
+    const [splitedTable, setSplitedTable] = useState<APIResponseTypes[][]>([])
+    
+    async function getData(){
+        try {
+            const data = await getAllPosts()
+            setTable(data)
+        } catch (error) {
+            console.log(error)
+        }
     }
-    useEffect(()=>{getData(1)},[])
+
+    function splitTableByLimit(limit: number = 10) {
+        if (table.length !== 0 && table !== undefined) {
+            let i = 0
+            let splitedArray: APIResponseTypes[][] = [];
+            while (table[i*limit] !== undefined) {
+                splitedArray = [...splitedArray, table.slice(i * limit, i * limit + limit)]
+                i++
+            }
+            setSplitedTable(splitedArray)
+        }
+    }
+
+    function setHomeSearchParams() {
+        if (searchParams.get('page') === null) {
+            setSearchParams('page=1')
+        } 
+    }
+    
+    useEffect(() => { splitTableByLimit(10) }, [table])
+    useEffect(() => {getData() ; setHomeSearchParams()},[])
+
 
     return(
         <div className="table">
             <Search/>
             <TableHeader/>
-            <button onClick={()=>{setSearchParams('jopa=1&pama=2')}}>Жми</button>
+            <button onClick={() => {console.log(splitedTable)}}>Жми</button>
             <button onClick={() => { setSearchParams('page=2') }}>2</button>
             <button onClick={() => { setSearchParams('page=3') }}>3</button>
         </div>
